802944c678152d5c7897f0c1207b80e911d991e3
Birendra Singh <birendrasingh7200@gmail.com>
2025-08-19 23:32:34 +0530

    dlet all

--- UAM/profileTime.py
+++
@@ -1,9 +0,0 @@
-import cProfile
-import pstats
-import data_loader_cli as dl
-
-with cProfile.Profile() as pr:
-    dl.main()  # Your main function
-
-stats = pstats.Stats(pr)
-stats.sort_stats(pstats.SortKey.TIME).print_stats(20)
--- streamlit_app/pages/dashboard.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def show():
-    st.title("📊 Dashboard")
-    st.write("Welcome to the Universal Analyst Model Dashboard!")
-    st.write("This page will provide an overview and key metrics.")
